ce93d2606ee5c75329ac4341abfcfa46b1adb2b3
LeMinux <107322540+LeMinux@users.noreply.github.com>
2023-03-10 03:20:04 +0000

    Replaced the .java and test4.c Files
    These were replaced to more represent real cases

--- Tests/test2.java
+++ Tests/test2.java
@@ -1,267 +1,197 @@
-package cpsc2150.extendedConnectX.models;
-
-/*
- *Initalization ensures:
- * A game board with a size of MAX_ROWS x MAX_COLS
- * that needs NUM_TO_WIN tokens in a row to win
- *
- * @constraints
- * rows = MAX_ROWS
- * columns = MAX_COLS
- * tokens needed to win = NUM_TO_WIN
- */
-public interface IGameBoard{
-
-	public final int MAX_ROWS = 9;
-	public final int MAX_COLS = 7;
-	public final int NUM_TO_WIN = 5;
-
-	/**
-	* gets the the number of rows from self
-	*
-	*@return the number of rows self has
-	*
-	*@post
-	*	|self| = #self
-	*	[rows of self] = #[rows of self]
-	*	[columns of self] = #[columns of self]
-	*/
-	public int getNumRows();
-
-	/**
-	* gets the the number of columsn from self
-	*
-	*@return the number of columns self has
-	*
-	*@post
-	*	|self| = #self
-	*	[rows of self] = #[rows of self]
-	*	[columns of self] = #[columns of self]
-	*/
-	public int getNumColumns();
-
-	/**
-	* returns how many tokens are needed for a win
-	*
-	*@return how many tokens are need in a row to win.
-		    this can be vertically, horizontally, or diagonally
-	*
-	*@post
-	*	|self| = #self
-	*	[rows of self] = #[rows of self]
-	*	[columns of self] = #[columns of self]
-	*/
-	public int getNumToWin();
-
-	/**
-	* Checks if column of self is free for another token
-	*
-	*@param col The column to check if free
-	*
-	*@return true if it can fit another token false if it can not fit
-	*	    another token
-	*
-	*@pre
-	*	0 {@code <=} col {@code <} MAX_COLS
-	*
-	*@post
-	*	[upper most row of self is empty]
-	*	|self| = #self
-	*	[rows of self] = #[rows of self]
-	*	[columns of self] = #[columns of self]
-	*/
-	public default boolean checkIfFree(int col){
-		BoardPosition top = new BoardPosition(MAX_ROWS - 1, col);
-		return (whatsAtPos(top) == ' ');
-	}
-
-
-	/**
-	* Places the token in the lowest row possible for that column
-	*
-	*@param player character representing the player
-	*@param col column to place the player token in
-	*
-	*@pre
-	*	checkIfFree() = true AND
-	*	player = 'X' OR player = 'O' AND
-	*	0 {@code <=} col {@code <} MAX_COLS
-	* 
-	*@post
-	*	[lowest row of self] = player
-	*/
-	public void placeToken(char player, int col);
-
-	/**
-	* Checks if the last token placed in a column results in a
-	* win.
-	*
-	*@param lastPlayed column of last played token
-	*
-	*@return true if result is a win of NUM_TO_WIN tokens false otherwise.
-	*
-	*@pre
-	*	0 {@code <=} lastPlayed {@code <} MAX_COLS AND
-	*	[lastPlayed is last played column]
-	*
-	*@post
-	*	board = #board
-	*	|self| = #self
-	*	[rows of self] = #[rows of self]
-	*	[columns of self] = #[columns of self]
-	*	checkForWin() iff [last played token results in a win]
-	*/
-	public default boolean checkForWin(int lastPlayed){
-		BoardPosition checkCell = new BoardPosition(MAX_ROWS - 1, lastPlayed);
-		while(whatsAtPos(checkCell) == ' '){
-			checkCell = new BoardPosition(MAX_ROWS - 1, --lastPlayed);
-		}
-		char player = whatsAtPos(checkCell);
-
-		return(checkHorizWin(checkCell,player) || checkVertWin(checkCell,player) || checkDiagWin(checkCell,player));
-	}
-
-	/**
-	* Checks to see if the game has resulted in a tie aka there are no free
-	* positions left
-	*
-	*@return True if all rows are occupied false otherwise
-	*
-	*@pre
-	*	checkforWin() = false AND
-	*	checkDiagWin() = false AND
-	*	checkVertWin() = false AND
-	*	checkHorizWin() = false
-	*
-	*@post
-	*	|self| = #self
-	*	[rows of self] = #[rows of self]
-	*	[columns of self] = #[columns of self]
-	*	checkTie() iff [top row of self is full]
-	*/
-	public default boolean checkTie(){
-		for(int c = 0; c < MAX_COLS; ++c){
-			if(checkIfFree(c) == true){
-				return false;
-			}
-		}
-		return true;
-	}
-
-	/**
-	* Checks to see if the last token results in a victory of 5 in a row
-	* horizontally
-	*
-	*@param cell Specific position of the board to check first
-	*@param player The player token to check for
-	*
-	*@return True if a horizontal win of NUM_TO_WIN tokens false otherwise
-	*
-	*@pre
-	*	player = 'X' OR player = 'O' AND
-	*	[cell is the last played cell]
-	*
-	*@post
-	*	|self| = #self
-	*	[rows of self] = #[rows of self]
-	*	[columns of self] = #[columns of self]
-	*	checkHorizWin iff [NUM_TO_WIN tokens of player in a row horizontally]
-	*/
-	public default boolean checkHorizWin(BoardPosition cell, char player){
-		//do{
-
-		//}while();
-		return false;
-	}
-
-	/**
-	* Checks to see if the last token results in a victory of 5 in a row
-	* vertically
-	*
-	*@param cell Specific position of the board to check first
-	*@param player The player token to check for
-	*
-	*@return True if a vertical win of NUM_TO_WIN tokens false otherwise
-	*
-	*@pre
-	*	player = 'X' OR player = 'O' AND
-	*	[cell is the position of the last played token]
-	*
-	*@post
-	*	|self| = #self
-	*	[rows of self] = #[rows of self]
-	*	[columns of self] = #[columns of self]
-	*	checkVertWin() iff [NUM_TO_WIN tokens of player in a row vertically]
-	*/
-	public default boolean checkVertWin(BoardPosition cell, char player){
-		//do{
-
-		//}while();
-		return false;
-	}
-
-	/**
-	* Checks to see if the last token results in a victory of 5 in a row
-	* diagonally
-	*
-	*@param cell Specific position of the board to check first
-	*@param player The player token to check for
-	*
-	*@return True if a diagonal win of NUM_TO_WIN tokens false otherwise
-	*
-	*@pre
-	*	player = 'X' OR player = 'O' AND
-	*	[cell is the position of the last played token]
-	*
-	*@post
-	*	|self| = #self
-	*	[rows of self] = #[rows of self]
-	*	[columns of self] = #[columns of self]
-	*	checkDiagWin() iff [NUM_TO_WIN tokens of player in a row diagonally]
-	*/
-	public default boolean checkDiagWin(BoardPosition cell, char player){
-		//do{
-
-		//}while();
-		return false;
-	}
-
-	/**
-	* Returns what player is at the specified position
-	*
-	*@param cell Specific position of the board to check for
-	*
-	*@return player token 'X' or player token 'O' or '' if empty
-	*
-	*@pre
-	*	[cell is valid position of self]
-	*
-	*@post
-	*	|self| = #self
-	*	[rows of self] = #[rows of self]
-	*	[columns of self] = #[columns of self]
-	*	(whatsAtPos() = 'X' iff [player in cell is 'X']) OR
-	*	(whatsAtPos() = 'Y' iff [player in cell is 'Y']) OR
-	*	(whatsAtPos() = '' iff [player in cell is ''])
-	*/
-	public char whatsAtPos(BoardPosition cell);
-
-	/**
-	* returns if a player is at the specified position
-	*
-	*@param cell The specified cell to check
-	*
-	*@return True if a player token is at the position false otherwise
-	*
-	*@pre
-	*	player = 'X' OR player = 'O'
-	*	[cell is a valid position on self]
-	*
-	*@post
-	*	|self| = #self
-	*	[rows of self] = #[rows of self]
-	*	[columns of self] = #[columns of self]
-	*	isPlayerAtPos() iff [player in the specified cell]
-	*/
-	public boolean isPlayerAtPos(BoardPosition cell, char player);
+//This class will hold operation that can be done to arrays
+public class ArrayOperations {
+    
+	//method for adding two MakeArrays together of any size
+    public static int [] addArray(MakeArray array1, MakeArray array2){
+        int [] sum;
+        
+        //this if statement is made to avoid a null point error
+        if (array1.getNumbersLength() >= array2.getNumbersLength()){
+        	//this statement executes when array1 is greater than or equal to array2
+            sum = new int[array1.getNumbersLength()];
+            
+            for(int k = 0; k < array2.getNumbersLength(); k++){
+                sum[k] = array1.getNumbersIndex(k) + array2.getNumbersIndex(k);
+            }
+            
+            for(int k = array2.getNumbersLength(); k < array1.getNumbersLength(); k++) {
+            	sum [k] = array1.getNumbersIndex(k);
+            }
+        }else{
+        	//this statement executes when array 1 is less than array 2
+            sum = new int[array2.getNumbersLength()];
+            
+            for(int k = 0; k < array1.getNumbersLength(); k++){
+                sum[k] = array1.getNumbersIndex(k) + array2.getNumbersIndex(k);
+            }
+        
+            for(int k = array1.getNumbersLength(); k < array2.getNumbersLength(); k++) {
+            	sum [k] = array2.getNumbersIndex(k);
+            }
+            
+            
+        }
+        return sum;
+    }
+    
+    
+    //overloaded method for adding two arrays together of any size
+    public static int [] addArray(int [] array1, int[] array2){
+        int larger = 0;
+        int smaller = 0;
+        int [] sum;
+        
+        //this if statement is made to avoid a null point error
+        if (array1.length >= array2.length){
+            smaller = array2.length;
+            larger = array1.length;
+            sum = new int[array1.length];
+            
+            for(int k = 0; k < smaller; k++){
+                sum[k] = array1[k] + array2[k];
+            }
+            
+            for(int k = smaller; k < larger; k++) {
+            	sum [k] = array1[k];
+            }
+        }else{
+            smaller = array1.length;
+            larger = array2.length;
+            sum = new int[array2.length];
+            
+            for(int k = 0; k < smaller; k++){
+                sum[k] = array1[k] + array2[k];
+            }
+        
+            for(int k = smaller; k < larger; k++) {
+            	sum [k] = array2[k];
+            }  
+        }
+        return sum;
+    }
+    
+  //overloaded method for adding two double arrays together of any size
+    public static double [] addArray(double [] array1, double[] array2){
+        int larger = 0;
+        int smaller = 0;
+        double [] sum;
+        
+        //this if statement is made to avoid a null point error
+        if (array1.length >= array2.length){
+            smaller = array2.length;
+            larger = array1.length;
+            sum = new double[array1.length];
+            
+            for(int k = 0; k < smaller; k++){
+                sum[k] = array1[k] + array2[k];
+            }
+            
+            for(int k = smaller; k < larger; k++) {
+            	sum [k] = array1[k];
+            }
+        }else{
+            smaller = array1.length;
+            larger = array2.length;
+            sum = new double[array2.length];
+            
+            for(int k = 0; k < smaller; k++){
+                sum[k] = array1[k] + array2[k];
+            }
+        
+            for(int k = smaller; k < larger; k++) {
+            	sum [k] = array2[k];
+            }  
+        }
+        return sum;
+    }
+    
+  //method for subtracting two MakeArrays together of any size
+    public static int [] subtractArray(MakeArray array1, MakeArray array2){
+        int [] difference;
+        
+        //this if statement is made to avoid a null point error
+        if (array1.getNumbersLength() >= array2.getNumbersLength()){
+        	//this statement executes when array1 is greater than or equal to array2
+            difference = new int[array1.getNumbersLength()];
+            
+            for(int k = 0; k < array2.getNumbersLength(); k++){
+                difference[k] = array1.getNumbersIndex(k) - array2.getNumbersIndex(k);
+            }
+            
+            for(int k = array2.getNumbersLength(); k < array1.getNumbersLength(); k++) {
+            	difference [k] = array1.getNumbersIndex(k);
+            }
+        }else{
+        	//this statement executes when array 1 is less than array 2
+            difference = new int[array2.getNumbersLength()];
+            
+            for(int k = 0; k < array1.getNumbersLength(); k++){
+                difference[k] = array1.getNumbersIndex(k) - array2.getNumbersIndex(k);
+            }
+        
+            for(int k = array1.getNumbersLength(); k < array2.getNumbersLength(); k++) {
+            	difference [k] = array2.getNumbersIndex(k);
+            }     
+        }
+        return difference;
+    }
+    
+  //overloaded method for subtracting two int arrays together of any size
+    public static int [] subtractArray(int [] array1, int[] array2){
+        int [] difference;
+        
+        //this if statement is made to avoid a null point error
+        if (array1.length >= array2.length){
+            difference = new int[array1.length];
+            
+            for(int k = 0; k < array2.length; k++){
+                difference[k] = array1[k] + array2[k];
+            }
+            
+            for(int k = array2.length; k < array1.length; k++) {
+            	difference[k] = array1[k];
+            }
+        }else{
+            difference = new int[array2.length];
+            
+            for(int k = 0; k < array1.length; k++){
+                difference[k] = array1[k] + array2[k];
+            }
+        
+            for(int k = array1.length; k < array2.length; k++) {
+            	difference [k] = array2[k];
+            }  
+        }
+        return difference;
+    }
+    
+  //overloaded method for subtracting two double arrays together of any size
+    public static double [] subtractArray(double [] array1, double[] array2){
+        double [] difference;
+        
+        //this if statement is made to avoid a null point error
+        if (array1.length >= array2.length){
+            difference = new double[array1.length];
+            
+            for(int k = 0; k < array2.length; k++){
+                difference[k] = array1[k] + array2[k];
+            }
+            
+            for(int k = array2.length; k < array1.length; k++) {
+            	difference[k] = array1[k];
+            }
+        }else{
+            difference = new double[array2.length];
+            
+            for(int k = 0; k < array1.length; k++){
+                difference[k] = array1[k] + array2[k];
+            }
+        
+            for(int k = array1.length; k < array2.length; k++) {
+            	difference [k] = array2[k];
+            }  
+        }
+        return difference;
+    }
+    
 }
--- Tests/test4.c
+++ Tests/test4.c
@@ -1,50 +1,37 @@
-/*
- * <student name>
- * <student section>
- * <student username>
- * <professor>
- */
+#include <stdio.h>
+#include <stdlib.h>
 
-void add(node_t **node, node_t **head){
-	node_t traverse = head;
-	while(head->nextEntry != NULL){
-		traverse = traverse->next;	
-	}
-	traverse->next = node;
-}
-
-node_t* readNodeInfo(FILE* input){
-	node_t entryLine; 
-	while(fscanf(input,"%[^,]%*c","%[^,]%*c","%[^,]%*c","%[^,]%*c",\
-			   "%[^,]%*c","%[^,]%*c","%[^,]%*c",\
-			   entryLine.last, entryLine.first,\
-			   entryLine.bDay.month,\
-			   entryLine.bDay.day,\
-			   entryLine.bDay.year,\
-			   entryLine.song,\
-			   entryLine.pastTime,\
-			   entryLine.reason)){
+char* dynamicString(FILE* inStream , char* message, int maxSize){
+	char* string;
+	char iterate;
+	int length = 0;
 	
+	string = realloc(NULL, sizeof(*string)*256);
+	if(!string)
+		return string;
+	
+	printf("%s", message);
+	while(EOF != (iterate=fgetc(inStream)) && iterate != '\n'){
+		string[length++] = iterate;
+		if(length == maxSize) return string;
 	}
-}                                              
-
-node_t* createList(FILE*, node_t**){
-
-}                                            
-
-void printList(FILE*, node_t*){
-	node_t traverse = node;
-	puts("LIST INFO:");
-	while(traverse->nextEntry != NULL){
-		printf("Name:%s\nDate of Birth:%s\nFavorite Song:%s\n"
-		       "");	
-	}	
-}                                                 
-
-void printBorder(FILE*){
-
+	string[length++] = '\0';
+	
+	return realloc(string, sizeof(*string)*length);
 }
 
-void deleteList(node_t**){
 
-} 
+int main(void){
+
+	//method 1 large buffer
+	char test1 [256];
+	printf("Enter anything you want ");
+	fgets(test1, 256, stdin);
+	printf("%s\n", test1);
+	
+	//method 2 reading per character through stdin
+	char* test2 = dynamicString(stdin, "Enter anything you want ", 256);
+	printf("%s\n", test2);
+	free(test2);
+	return 0;
+}
